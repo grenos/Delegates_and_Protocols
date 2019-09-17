9dca81778165f759d7a807b8ae497d41f4ee2556
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-09-17 12:16:52 +0200

    up

--- Delegates_and_Protocols/SecondViewController.swift
+++ Delegates_and_Protocols/SecondViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
+// to send data back we need to use protocols and delegates
+// a protocol doesn't do anything but to set the rules of
+// what a delegate should/could do
+// kinda like an interface in typescript
+/// 1)
+protocol CanReceive {
+    func dataReceived(data: String)
+}
+
+
 class SecondViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textFiled: UITextField!
     
+    // we declare a property (var) and asign the CanReceive Protocol
+    // as its type
+    // this var will do the delegating
+    // use the ? in case there is no delegate class declared
+    /// 4)
+    var delegate: CanReceive?
+    
+    // to save the data that will be passed over from the other controller
+    var data = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // pass the data recieved from the other view controller
+        // as the text of the label
+        label.text = data
+        
     }
     
     @IBAction func sendDataBack(_ sender: UIButton) {
+        // on button click call the function
+        // declared in the protocol and placed
+        // on the delegate class
+        // send data = what user has typed in textfiled
+        // if delegate value is nil this func will do NOTHING
+        /// 5)
+        delegate?.dataReceived(data: textFiled.text!)
+        /// 7) OPTIONAL!
+        // dismiss current view and go back to first view
+        dismiss(animated: true, completion: nil)
     }
     
 }
--- Delegates_and_Protocols/ViewController.swift
+++ Delegates_and_Protocols/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+                                        /// 2)
+class ViewController: UIViewController, CanReceive {
 
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textFiled: UITextField!
@@ -19,6 +19,35 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendButtonPressed(_ sender: UIButton) {
+        // on button click transition to the other page
+        // using the "sendDataFwd" segue with sender (self) this class
+        performSegue(withIdentifier: "sendDataFwd", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // every time we create a segue we have to go to the right panel
+        // and give it an identifier name in the attributes inspector
+        if segue.identifier == "sendDataFwd" {
+            // we tap into the destination of the segue
+            // and we inform our compiler that the distination
+            // is of type "SecondViewController"
+            let secondVC = segue.destination as! SecondViewController
+            // access the data property (var) of the SecondViewController
+            // and pass whatever the user types on the textfiled input
+            secondVC.data = textFiled.text!
+            // the last step is to set the self (this class)
+            // as the delgate of the protocol
+            /// 6)
+            secondVC.delegate = self
+        }
+    }
+    
+    // once we set this class as the delegate with the protocol
+    // we need to decalre the function and/or other things
+    // that the protocol dictates that this class should do
+    /// 3)
+    func dataReceived(data: String) {
+        label.text = data
     }
     
 }
