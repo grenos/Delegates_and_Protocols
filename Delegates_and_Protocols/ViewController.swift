2836cd6b0de8f2a9077034e70c4826d04124d12b
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-09-17 15:10:35 +0200

    doc and info

--- Delegates_and_Protocols/ViewController.swift
+++ Delegates_and_Protocols/ViewController.swift
@@ -42,6 +42,7 @@ class ViewController: UIViewController, CanReceive {
         }
     }
     
+    // DELEGATE METHOD
     // once we set this class as the delegate with the protocol
     // we need to decalre the function and/or other things
     // that the protocol dictates that this class should do
@@ -52,3 +53,25 @@ class ViewController: UIViewController, CanReceive {
     
 }
 
+/// ************************************************************************
+
+// Delegation is a design pattern that enables a class to hand off (or “delegate”) some of its responsibilities to an instance of another class.
+// https://learnappmaking.com/delegation-swift-how-to/
+// In short, it’s a great way to “hook into” events that happen within code you don’t control.
+
+
+/// https://subclassing
+// An alternative to delegation is subclassing.
+// This has a massive disadvantage: you inherit the entire CLLocationManager class, for something as simple as getting a bit of location data. You would need to override some of its default functionality, which you either have to call directly with super or replace entirely.
+
+// And lastly, subclassing creates a tightly-coupled class hierarchy, which doesn’t make sense unless your subclass is similar in nature to the class you’re subclassing. This is unlikely if you’re merely responding to GPS location updates.
+
+
+/// https://Observable
+// The Observable pattern is useful when your code needs to communicate with multiple components, with a one-to-many or many-to-many relationship. One component in your app broadcasts a signal that multiple other components respond to. And apart from a broadcast type and some associated data, you can’t formalize the requirements for the communication like a protocol can.
+
+
+///  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Delegation is more lightweight than subclassing, because you don’t have to inherit a complete class or struct
+// Delegation is useful for 1-on-1 relationships, whereas the Observer pattern is more suitable for one-to-many and many-to-many relationships.
+// Delegation is flexible, because it doesn’t require the delegating class to know anything at all about a delegate – only that it conforms to a protocol
